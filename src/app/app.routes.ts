108904b384fdaa80e4ec0d476a4c9c59733e6c29
Fermin <ferminfernandez97@gmail.com>
2018-06-25 12:06:16 -0400

    Routes + Home and About Components
    The Home route, Guitars route & About  routes was created and they are already working.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent} from './components/home/home.component';
+import { AboutComponent} from './components/about/about.component';
+import { GuitarsComponent} from './components/guitars/guitars.component';
 
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'guitars', component: GuitarsComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
